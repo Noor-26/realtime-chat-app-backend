c5949508d45ecd7042ab0fcd660b6aabe96300d0
Noor-26 <nk26982698@gmail.com>
2022-07-16 19:14:07 +0600

    all data fetched

--- index.js
+++ index.js
@@ -6,6 +6,24 @@ app.use(cors())
 app.use(express.json())
 const port = process.env.PORT || 5000
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://Noor-26:noor123228@cluster0.296sc.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async () => { 
+    try{
+        await client.connect()
+        const chatCollection = client.db("Chat-app").collection("chats");   
+        
+        app.get('/chats',async (req,res) => {
+            const chats = await chatCollection.find().toArray()
+            res.send(chats);
+        })
+    }
+
+    finally{}
+}
+run()
 app.get('/', async (req,res) => {
     res.send('server is runing')
 })
